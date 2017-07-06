074561fbc34e61ab309560e199084f1a28fb8e09
谭宇 <tanyucn@outlook.com>
2017-07-06 18:09:43 +0800

    before_action的操作，集成编辑 删除 上传

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -14,11 +15,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "没有操作权限"
-    end
+    find_group_and_check_permission
   end
 
   def create
@@ -33,11 +30,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "没有操作权限"
-    end
+    find_group_and_check_permission
 
     @group.update(group_params)
       redirect_to groups_path, notice: "上传成功"
@@ -47,18 +40,23 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
+    find_group_and_check_permission
 
-    if current_user != @group.user
-      redirect_to root_path, alert: "没有操作权限"
-    end
-    
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
   end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @gruop.user
+      redirect_to root_path, alert: "没有操作权限"
+    end
+
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
